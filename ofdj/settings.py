e4a723fecd1f3ddcbf1a2138f95ce21e6d169fde
Serhii <serge.sergeevich@gmail.com>
2016-02-12 18:31:44 +0200

    Settings: DB

--- ofdj/settings.py
+++ ofdj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'ofdj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polls',
+        'USER': 'polls',
+        'PASSWORD': 'polls',
+        'HOST': '127.0.0.1',
     }
 }
 
