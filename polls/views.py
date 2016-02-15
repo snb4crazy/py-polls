0c1def0769ffae8f0c4800db36a4163e4e2e0ed7
Serhii <serge.sergeevich@gmail.com>
2016-02-15 12:52:38 +0200

    Polls:Views: Integrate Render

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def detail(request, question_id):
@@ -23,9 +18,6 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
