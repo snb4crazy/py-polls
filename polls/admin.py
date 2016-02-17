f274b554cdd35555118a5d0f9c61a147f2088eeb
Serhii <serge.sergeevich@gmail.com>
2016-02-17 20:02:14 +0200

    Polls:AdminPanel:Customization: Adding Related Objects

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    """
+    Args:
+        admin.StackedInline: input per row,
+        admin.Tabular - all props of model in row
+    """
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
